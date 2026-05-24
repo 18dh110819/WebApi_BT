2a6eb8dbcc0d79a5bdae4366743af2b3e6ecb9fe
18dh110819 <nguyenhoanganhv02@gmail.com>
2026-05-25 00:06:39 +0700

    Add files

---
+++ backend/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.OpenApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "My API",
+        Version = "v1",
+        Description = "API documentation for .NET 10"
+    });
+});
+
+var app = builder.Build();
+
+app.MapControllers();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+        options.RoutePrefix = "swagger";
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
